89fc44ce42d9881862948e6a24deaa4c33dd41a2
XxMrTyronexX <80863499+XxMrTyronexX@users.noreply.github.com>
2023-06-11 22:13:41 +0930

    Update Colours.hpp

--- Colours.hpp
+++ Colours.hpp
@@ -1,11 +1,1 @@
-#ifndef COLOURS_H
-#define COLOURS_H
 
-#define BLACK 0x0000
-#define WHITE 0xFFFF
-#define BUTTON_COLOUR 0x07FF
-
-#define TEXT_COLOUR BLACK
-#define BACKGROUND_COLOUR WHITE
-
-#endif
